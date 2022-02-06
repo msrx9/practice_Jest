7b97ec2da6c4687ee8979c787139b72470755ee8
msrx9 <msrx9g@gmail.com>
2022-02-06 16:31:41 +0900

    Add number test

--- tests/matcher.test.js
+++ tests/matcher.test.js
@@ -1,17 +1,17 @@
-describe('General matcher test sample', () => {
-  test('two plus two is four', () => {
+describe("General matcher test sample", () => {
+  test("two plus two is four", () => {
     // .toBe() is '==='
     expect(2 + 2).toBe(4);
   });
 
-  test('object assignment', () => {
-    const data = {one: 1};
-    data['two'] = 2;
+  test("object assignment", () => {
+    const data = { one: 1 };
+    data["two"] = 2;
     // .toEqual() does recursive check field object or array.
-    expect(data).toEqual({one: 1, two: 2});
+    expect(data).toEqual({ one: 1, two: 2 });
   });
 
-  test('adding positive numbers is not zero', () => {
+  test("adding positive numbers is not zero", () => {
     for (let a = 1; a < 10; a++) {
       for (let b = 1; b < 10; b++) {
         // The test have to clear for all a + b execute.
@@ -21,12 +21,12 @@ describe('General matcher test sample', () => {
   });
 });
 
-describe('bool and like that value', () => {
-  test('null', () => {
+describe("bool and like that value", () => {
+  test("null", () => {
     const n = null;
     // .toBeNull() does hit only null.
     expect(n).toBeNull();
-    // .toBeDefined() does hit only undefined. 
+    // .toBeDefined() does hit only undefined.
     expect(n).toBeDefined();
     // .toBeUndefined() does opposite of .toBeDefined().
     expect(n).not.toBeUndefined();
@@ -36,12 +36,26 @@ describe('bool and like that value', () => {
     expect(n).toBeFalsy();
   });
 
-  test('zero', () => {
+  test("zero", () => {
     const z = 0;
     expect(z).not.toBeNull();
     expect(z).toBeDefined();
     expect(z).not.toBeUndefined();
     expect(z).not.toBeTruthy();
     expect(z).toBeFalsy();
+  });
+});
+
+describe("number", () => {
+  test("two plus two", () => {
+    const value = 2 + 2;
+    expect(value).toBeGreaterThan(3);
+    expect(value).toBeGreaterThanOrEqual(3.5);
+    expect(value).toBeLessThan(5);
+    expect(value).toBeLessThanOrEqual(4.5);
+
+    // toBe and toEqual are equivalent for numbers
+    expect(value).toBe(4);
+    expect(value).toEqual(4);
   });
 });
