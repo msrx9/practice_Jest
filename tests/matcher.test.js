b44f079d4ced382d21782efd6e8d09282b5374cb
msrx9 <msrx9g@gmail.com>
2022-02-06 17:09:02 +0900

    Update use double quote -> single quote

--- tests/matcher.test.js
+++ tests/matcher.test.js
@@ -1,17 +1,17 @@
-describe("General matcher test sample", () => {
-  test("two plus two is four", () => {
+describe('General matcher test sample', () => {
+  test('two plus two is four', () => {
     // .toBe() is '==='
     expect(2 + 2).toBe(4);
   });
 
-  test("object assignment", () => {
+  test('object assignment', () => {
     const data = { one: 1 };
-    data["two"] = 2;
+    data['two'] = 2;
     // .toEqual() does recursive check field object or array.
     expect(data).toEqual({ one: 1, two: 2 });
   });
 
-  test("adding positive numbers is not zero", () => {
+  test('adding positive numbers is not zero', () => {
     for (let a = 1; a < 10; a++) {
       for (let b = 1; b < 10; b++) {
         // The test have to clear for all a + b execute.
@@ -21,8 +21,8 @@ describe("General matcher test sample", () => {
   });
 });
 
-describe("bool and like that value", () => {
-  test("null", () => {
+describe('bool and like that value', () => {
+  test('null', () => {
     const n = null;
     // .toBeNull() does hit only null.
     expect(n).toBeNull();
@@ -30,13 +30,13 @@ describe("bool and like that value", () => {
     expect(n).toBeDefined();
     // .toBeUndefined() does opposite of .toBeDefined().
     expect(n).not.toBeUndefined();
-    // .toBeTruthy() does hit "if" statement is true.
+    // .toBeTruthy() does hit 'if' statement is true.
     expect(n).not.toBeTruthy();
-    // .toBeFalsy() does hit "if" statement is false.
+    // .toBeFalsy() does hit 'if' statement is false.
     expect(n).toBeFalsy();
   });
 
-  test("zero", () => {
+  test('zero', () => {
     const z = 0;
     expect(z).not.toBeNull();
     expect(z).toBeDefined();
@@ -46,8 +46,8 @@ describe("bool and like that value", () => {
   });
 });
 
-describe("number", () => {
-  test("two plus two", () => {
+describe('number', () => {
+  test('two plus two', () => {
     const value = 2 + 2;
     expect(value).toBeGreaterThan(3);
     expect(value).toBeGreaterThanOrEqual(3.5);
@@ -59,35 +59,35 @@ describe("number", () => {
     expect(value).toEqual(4);
   });
 
-  test("adding floating point numbers", () => {
+  test('adding floating point numbers', () => {
     const value = 0.1 + 0.2;
     // expect(value).toBe(0.3) does not work as expcted.
     expect(value).toBeCloseTo(0.3);
   });
 });
 
-describe("strings", () => {
-  test("there is no I in item", () => {
-    expect("team").not.toMatch(/I/);
+describe('strings', () => {
+  test('there is no I in item', () => {
+    expect('team').not.toMatch(/I/);
   });
 
   test('but there is a "stop" in Chirstoph', () => {
-    expect("Chirstoph").toMatch(/stop/);
+    expect('Chirstoph').toMatch(/stop/);
   });
 });
 
-describe("array and repeatbale objects", () => {
+describe('array and repeatbale objects', () => {
   const shoppingList = [
-    "diapers",
-    "kleenex",
-    "trash bags",
-    "paper towel",
-    "milk",
+    'diapers',
+    'kleenex',
+    'trash bags',
+    'paper towel',
+    'milk',
   ];
 
-  test("the shopping list has milk on it", () => {
-    expect(shoppingList).toContain("milk");
+  test('the shopping list has milk on it', () => {
+    expect(shoppingList).toContain('milk');
     // new Set() does make unique list.
-    expect(new Set(shoppingList)).toContain("milk");
+    expect(new Set(shoppingList)).toContain('milk');
   });
 });
