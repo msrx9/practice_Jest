f723839a7f60ec8e0c9e2ae9374ac2703d7d33bc
msrx9 <msrx9g@gmail.com>
2022-02-06 11:34:45 +0900

    Add matcher test

--- tests/matcher.test.js
+++ tests/matcher.test.js
@@ -2,4 +2,10 @@ describe('General matcher test sample', () => {
   test('two plus two is four', () => {
     expect(2 + 2).toBe(4);
   });
+
+  test('object assignment', () => {
+    const data = {one: 1};
+    data['two'] = 2;
+    expect(data).toEqual({one: 1, two: 2});
+  });
 });
