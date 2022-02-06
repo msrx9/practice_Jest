d395cfd45b8e7c343f7c762d7ff532fc04c4e2f7
msrx9 <msrx9g@gmail.com>
2022-02-06 11:05:00 +0900

    Grouped tests

--- tests/matcher.test.js
+++ tests/matcher.test.js
@@ -1,3 +1,5 @@
-test('two plus two is four', () => {
-  expect(2 + 2).toBe(4);
+describe('General matcher test sample', () => {
+  test('two plus two is four', () => {
+    expect(2 + 2).toBe(4);
+  });
 });
